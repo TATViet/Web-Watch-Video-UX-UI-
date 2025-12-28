70f2d318f5bc4131c3277efceb799884c2805181
TATViet <trananhtracviet20052011@gmail.com>
2025-12-28 16:17:55 +0700

    Customize the database for locala users

--- config/db.js
+++ config/db.js
@@ -1,12 +1,36 @@
+// const { Pool } = require("pg");
+// // Admin pool (kết nối đến DB "postgres" cho admin tasks)
+// const adminPool = new Pool({
+// connectionString: "postgres://owlloop_user:uflgatlmOGErCp2ex3rEtPAbdfjWZCZF@dpg-d545b9h5pdvs73fth0hg-a:5432/postgres",
+// ssl: { rejectUnauthorized: false }, // Bật SSL cho Render
+// });
+// // App pool (kết nối đến DB "owlloop")
+// const appPool = new Pool({
+// connectionString: "postgres://owlloop_user:uflgatlmOGErCp2ex3rEtPAbdfjWZCZF@dpg-d545b9h5pdvs73fth0hg-a:5432/owlloop",
+// ssl: { rejectUnauthorized: false }, // Bật SSL cho Render
+// });
+// module.exports = { adminPool, appPool };
+
+
+
 const { Pool } = require("pg");
-// Admin pool (kết nối đến DB "postgres" cho admin tasks)
+
+// Pool dùng để tạo database (connect vào postgres)
 const adminPool = new Pool({
-connectionString: "postgres://owlloop_user:uflgatlmOGErCp2ex3rEtPAbdfjWZCZF@dpg-d545b9h5pdvs73fth0hg-a:5432/postgres",
-ssl: { rejectUnauthorized: false }, // Bật SSL cho Render
+  user: "postgres",
+  password: "vietk12+",
+  host: "localhost",
+  port: 5432,
+  database: "postgres"
 });
-// App pool (kết nối đến DB "owlloop")
+
+// Pool dùng cho app chính
 const appPool = new Pool({
-connectionString: "postgres://owlloop_user:uflgatlmOGErCp2ex3rEtPAbdfjWZCZF@dpg-d545b9h5pdvs73fth0hg-a:5432/owlloop",
-ssl: { rejectUnauthorized: false }, // Bật SSL cho Render
+  user: "postgres",
+  password: "vietk12+",
+  host: "localhost",
+  port: 5432,
+  database: "owlloop"
 });
+
 module.exports = { adminPool, appPool };
